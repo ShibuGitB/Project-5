9d8c8f7af730aa12d1beda6a0968bd08550b0a83
shibu b <shibukalathil2003@gmail.com>
2024-06-20 18:09:54 +0530

    third commit

--- Category3.py
+++ Category3.py
@@ -14,44 +14,50 @@ def app23() :
     review=st.text_input("Review"":pencil:","type here") 
     st.caption("Rating"" "":star2:")
     
-    if review!="type here" : 
+    try : 
         
-        input=[review] 
-    
-        model=tf.keras.models.load_model("NetFlix Review Rating Model-2.h5") 
-        vectorizer=joblib.load("NetFlix Vectorizer")
-        encoder=joblib.load("NetFlix Encoder") 
-    
-        rating=model.predict(vectorizer.transform(input)) 
-    
-        position=rating.argmax(axis=1) 
-    
-        rating=encoder.inverse_transform(position) 
-    
-        if rating==1 : 
+        if review!="type here" : 
         
-            image=Image.open("rating images/1 star rating.png")
-            st.image(image,width=390) 
-            st.caption("By your Review to this show, Your rating is 1 for this. Please suggest your type of shows then we can easily gave you the correct one as your taste !!!")
+            input=[review] 
+    
+            model=tf.keras.models.load_model("NetFlix Review Rating Model-2.h5") 
+            vectorizer=joblib.load("NetFlix Vectorizer")
+            encoder=joblib.load("NetFlix Encoder") 
+    
+            rating=model.predict(vectorizer.transform(input)) 
+    
+            position=rating.argmax(axis=1) 
+    
+            rating=encoder.inverse_transform(position) 
+    
+            if rating==1 : 
         
-        elif rating==2 : 
+                image=Image.open("rating images/1 star rating.png")
+                st.image(image,width=390) 
+                st.caption("By your Review to this show, Your rating is 1 for this. Please suggest your type of shows then we can easily gave you the correct one as your taste !!!")
         
-            image=Image.open("rating images/2 star rating.png")
-            st.image(image,width=390) 
-            st.caption("By your Review to this show, Your rating is 1 for this. Please suggest your type of shows then we can easily gave you the correct one as your taste !!!")
+            elif rating==2 : 
+        
+                image=Image.open("rating images/2 star rating.png")
+                st.image(image,width=390) 
+                st.caption("By your Review to this show, Your rating is 1 for this. Please suggest your type of shows then we can easily gave you the correct one as your taste !!!")
             
-        elif rating==3 : 
+            elif rating==3 : 
         
-            image=Image.open("rating images/3 star rating.png")
-            st.image(image,width=390) 
-            st.caption("By your Review to this show, Your rating is 3 for this, ThankYouuuu at the Same time suggest your type to reach you taste !!!")
+                image=Image.open("rating images/3 star rating.png")
+                st.image(image,width=390) 
+                st.caption("By your Review to this show, Your rating is 3 for this, ThankYouuuu at the Same time suggest your type to reach you taste !!!")
         
-        elif rating==4 : 
+            elif rating==4 : 
         
-            image=Image.open("rating images/4 star rating.png")
-            st.image(image,width=390,caption="By your Review to this show, Your rating is 4 for this: ThankYouuuuuuuuu !!!")    
+                image=Image.open("rating images/4 star rating.png")
+                st.image(image,width=390,caption="By your Review to this show, Your rating is 4 for this: ThankYouuuuuuuuu !!!")    
         
-        elif rating==5 : 
+            elif rating==5 : 
         
-            image=Image.open("rating images/5 star rating.png")
-            st.image(image,width=500,caption="By your Review to this show, Your rating is 5 for this: Hoooreyyyyyyyyy ThankYouuuuuuuuuuu!!!")    
+                image=Image.open("rating images/5 star rating.png")
+                st.image(image,width=500,caption="By your Review to this show, Your rating is 5 for this: Hoooreyyyyyyyyy ThankYouuuuuuuuuuu!!!")    
+                
+    except : 
+        
+        st.caption("Put Your Review there , for the Rating :writing_hand:") 
